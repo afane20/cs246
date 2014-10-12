6cf2eb4ada649d60c458e2c4e8586a559862c2ba
Ernesto Afane <salvador.afane@gmail.com>
2014-10-11 19:46:07 -0600

    Here is the hello world program

---
+++ src/Output/Output.java
@@ -0,0 +1,12 @@
+package Output;
+
+import java.io.PrintStream;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+public class Output{
+    public static void main(String[] args) {
+    System.out.println("hello world");
+}
+
+}
