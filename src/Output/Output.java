65c0c84c11eb6818557e6ffdf54ce9c4c7f8fb12
Ernesto Afane <salvador.afane@gmail.com>
2014-10-11 20:34:05 -0600

    Here is the hello world program

--- src/Output/Output.java
+++ src/Output/Output.java
@@ -7,6 +7,7 @@ import javafx.scene.layout.BorderPane;
 public class Output{
     public static void main(String[] args) {
     System.out.println("hello world");
+    System.out.println("Hello Ashlie");
 }
 
 }
